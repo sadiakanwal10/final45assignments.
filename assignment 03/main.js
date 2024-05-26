ea7a58650b454db0f09c0dd2e8a8e1e28a2d3394
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-05-26 04:59:25 -0700

    first commit

---
+++ assignment 03/main.js
@@ -0,0 +1,5 @@
+// storing the famous quote and its author //
+var authorName = "Albert Einstein";
+var famousQuote = "Life is like riding a bicycle.To keep your balance, you must keep moving";
+// print the quote and its author name //
+console.log("".concat(authorName, " once said, \"").concat(famousQuote, "\""));
---
+++ assignment 03/main.ts
@@ -0,0 +1,8 @@
+// storing the famous quote and its author //
+ 
+let authorName:string = "Albert Einstein";
+let famousQuote:string = "Life is like riding a bicycle.To keep your balance, you must keep moving";
+
+// print the quote and its author name //
+
+console.log(`${authorName} once said, "${famousQuote}"`);
