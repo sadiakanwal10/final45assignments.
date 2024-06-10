7b9616d7a533dfcc16130bbe5af9e370a94362b2
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-06-09 23:52:21 -0700

    first commit

---
+++ assignment no 12/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// BY FOR EACH METHOD //
+let tranportationMode = ["honda bicycle", "car", "bus", "train", "aeroplane"];
+tranportationMode.forEach(mode => {
+    console.log(`"I would like to own a ${mode}"`);
+});
---
+++ assignment no 12/main.ts
@@ -0,0 +1,6 @@
+// BY FOR EACH METHOD //
+
+let tranportationMode:string[]= ["honda bicycle","car","bus","train","aeroplane"];
+tranportationMode.forEach(mode => {console.log(`"I would like to own a ${mode}"`);
+    
+});
