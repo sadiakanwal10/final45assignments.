753e134d734239fefe0285cc75881dee68a184eb
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-06-09 23:30:17 -0700

    first commit

---
+++ assignment  no 11/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// BY for loop method //
+let names = ["ayesha", "armish", "eman", "mahnoor", "dua"];
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello! ${names[i]} How are you?`);
+}
---
+++ assignment  no 11/main.ts
@@ -0,0 +1,7 @@
+// BY for loop method //
+let names:string[]= ["ayesha","armish","eman","mahnoor","dua"];
+for (let i = 0; i < names.length; i++) {
+    console.log (`Hello! ${names[i]} How are you?`);
+    
+}
+    
